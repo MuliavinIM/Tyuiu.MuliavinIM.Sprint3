7a763b05f2ed6a784bc916aae56e25340ca9312b
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-11-28 15:35:33 +0500

    Task1

---
+++ Tyuiu.MuliavinIM.Sprint3.Task1.V11.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MuliavinIM.Sprint3.Task1.V11.Lib;
+
+namespace Tyuiu.MuliavinIM.Sprint3.Task1.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            double res = ds.GetMultiplySeries(0.75, 1, 2);
+            double wait = 1.328;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
