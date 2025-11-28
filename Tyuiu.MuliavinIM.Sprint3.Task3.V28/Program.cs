42303693cea098ab73752f994e1b1aa62f5df25d
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-11-28 15:57:59 +0500

    Task3

--- Tyuiu.MuliavinIM.Sprint3.Task3.V28/Program.cs
+++ Tyuiu.MuliavinIM.Sprint3.Task3.V28/Program.cs
@@ -1,18 +1,18 @@
-﻿using Tyuiu.MuliavinIM.Sprint3.Task2.V10.Lib;
-namespace Tyuiu.MuliavinIM.Sprint3.Task2.V10
+﻿using Tyuiu.MuliavinIM.Sprint3.Task3.V28.Lib;
+
+namespace Tyuiu.MuliavinIM.Sprint3.Task3.V28
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            DataService ds = new DataService();
             Console.Title = "Спринт #3 | Выполнил: Мулявин И.М. | ИСТНб-25-1";
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
             Console.WriteLine("* Тема:                                                                   *");
-            Console.WriteLine("* Задание #2                                                              *");
-            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("*                                                                         *");
@@ -20,18 +20,14 @@ namespace Tyuiu.MuliavinIM.Sprint3.Task2.V10
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            int value = 1;
-            int startValue = 1;
-            int stopValue = 13;
+            string value = "f35hyt t4j 3gkg45";
+            char replaceChar = 'r';
 
+            var ds = new DataService();
+            string result = ds.ReplaceNumOnChar(value, replaceChar);
 
-
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
-            Console.WriteLine("***************************************************************************");
-
-            Console.WriteLine(ds.GetMultiplySeries(value, startValue, stopValue));
-            Console.ReadKey();
+            Console.WriteLine($"Исходная строка: {value}");
+            Console.WriteLine($"Результат:       {result}");
         }
     }
 }
