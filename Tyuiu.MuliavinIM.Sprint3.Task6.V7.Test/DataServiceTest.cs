f379f515b0a6fb82179d485c0bcec096a3d5117a
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-11-28 21:57:16 +0500

    Task6

---
+++ Tyuiu.MuliavinIM.Sprint3.Task6.V7.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MuliavinIM.Sprint3.Task6.V7.Lib;
+
+namespace Tyuiu.MuliavinIM.Sprint3.Task6.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 17;
+            int stopValue = 26;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 344;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
